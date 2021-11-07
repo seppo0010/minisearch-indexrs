c8b4a6dc0da7de8d15b514fdfc140316d79726a9
Sebastian Waisbrot <seppo0010@gmail.com>
2021-11-07 09:02:14 -0300

    minisearch js displays json result

--- minisearch-cli/main.js
+++ minisearch-cli/main.js
@@ -6,8 +6,7 @@ const sourcePath = process.argv[3]
 
 const config = JSON.parse(fs.readFileSync(configPath))
 
-for (let x = 0; x < 100; x++) {
-  const source = JSON.parse(fs.readFileSync(sourcePath))
-  const ms = new MiniSearch(config)
-  ms.addAll(source)
-}
+const source = JSON.parse(fs.readFileSync(sourcePath))
+const ms = new MiniSearch(config)
+ms.addAll(source)
+console.log(JSON.stringify(ms.toJSON()))
