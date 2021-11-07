beece3fa867a2dba56444c0ba5458684e8d71d00
Sebastian Waisbrot <seppo0010@gmail.com>
2021-11-07 16:00:14 -0300

    Refactoring - extract map_json

--- src/main.rs
+++ src/main.rs
@@ -32,18 +32,20 @@ fn field_ids_json(field_ids_src: HashMap<String, usize>) -> JSONMap<String, JSON
     return field_ids;
 }
 
-fn average_field_length_json(field_num_tokens: HashMap<usize, usize>, next_id: f64) -> JSONMap<String, JSONValue> {
+fn average_field_length_json(
+    field_num_tokens: HashMap<usize, usize>,
+    next_id: f64,
+) -> JSONMap<String, JSONValue> {
     let mut average_field_length = JSONMap::new();
     for (field_id, num_tokens) in field_num_tokens.into_iter() {
-        average_field_length.insert(
-            field_id.to_string(),
-            (num_tokens as f64 / next_id).into(),
-        );
+        average_field_length.insert(field_id.to_string(), (num_tokens as f64 / next_id).into());
     }
     average_field_length
 }
 
-fn field_length_json(field_length_src: HashMap<usize, HashMap<usize, usize>>) -> JSONMap<String, JSONValue> {
+fn field_length_json(
+    field_length_src: HashMap<usize, HashMap<usize, usize>>,
+) -> JSONMap<String, JSONValue> {
     let mut field_length = JSONMap::new();
     for (small_id, field_lengths) in field_length_src.into_iter() {
         let mut json_field_lengths = JSONMap::new();
@@ -53,6 +55,68 @@ fn field_length_json(field_length_src: HashMap<usize, HashMap<usize, usize>>) ->
         field_length.insert(small_id.to_string(), json_field_lengths.into());
     }
     field_length
+}
+
+fn map_json(map: PatriciaMap<Vec<(usize, usize)>>) -> JSONMap<String, JSONValue> {
+    let node = Node::from(map);
+
+    let mut index = JSONMap::new();
+    index.insert("_prefix".to_string(), "".into());
+    let mut stack = vec![("".to_owned(), JSONMap::new())];
+    for (level, node) in node.into_iter() {
+        let label = std::str::from_utf8(node.label()).unwrap().to_owned();
+        if level == 0 {
+            continue;
+        }
+        while level + 1 <= stack.len() {
+            let level = stack.len() - 2;
+            let (key, val) = stack.pop().unwrap();
+            stack[level].1.insert(key, val.into());
+        }
+        let mut val = JSONMap::new();
+        if level + 1 > stack.len() {
+            if let Some(nodes) = node.value() {
+                let mut tree = HashMap::new();
+                for (small_id, field_id) in nodes {
+                    let subtree = match tree.entry(field_id) {
+                        Entry::Occupied(o) => o.into_mut(),
+                        Entry::Vacant(v) => v.insert(HashMap::<usize, usize>::new()),
+                    };
+                    match subtree.entry(*small_id) {
+                        Entry::Occupied(o) => *o.into_mut() += 1,
+                        Entry::Vacant(v) => {
+                            v.insert(1);
+                        }
+                    };
+                }
+                for (small_id, counts) in tree.into_iter() {
+                    let df = counts.len();
+                    let mut ds = JSONMap::new();
+                    for (field_id, count) in counts.into_iter() {
+                        ds.insert(field_id.to_string(), count.into());
+                    }
+                    val.insert(
+                        "".to_string(),
+                        json!({
+                            small_id.to_string(): {
+                                "df": df,
+                                "ds": ds,
+                            }
+                        })
+                        .into(),
+                    );
+                }
+            }
+            stack.push((label, val));
+        }
+    }
+    while stack.len() > 1 {
+        let level = stack.len() - 2;
+        let (key, val) = stack.pop().unwrap();
+        stack[level].1.insert(key, val.into());
+    }
+    index.insert("_tree".to_string(), stack.pop().unwrap().1.into());
+    index
 }
 
 fn process_term(term: &str) -> String {
@@ -148,74 +212,21 @@ impl Index {
         h.insert("documentCount".to_string(), self.next_id.into());
         h.insert("nextId".to_string(), self.next_id.into());
         h.insert("documentIds".to_string(), self.document_ids.into());
-        h.insert("fieldIds".to_string(), field_ids_json(self.field_ids).into());
+        h.insert(
+            "fieldIds".to_string(),
+            field_ids_json(self.field_ids).into(),
+        );
         h.insert(
             "averageFieldLength".to_string(),
-            average_field_length_json(self.field_num_tokens, self.next_id as f64).into()
+            average_field_length_json(self.field_num_tokens, self.next_id as f64).into(),
         );
-        h.insert("fieldLength".to_string(), field_length_json(self.field_length).into());
+        h.insert(
+            "fieldLength".to_string(),
+            field_length_json(self.field_length).into(),
+        );
+        h.insert("index".to_string(), map_json(self.map).into());
 
         // TODO: storedFields
-
-        let node = Node::from(self.map);
-
-        let mut index = JSONMap::new();
-        index.insert("_prefix".to_string(), "".into());
-        let mut stack = vec![("".to_owned(), JSONMap::new())];
-        for (level, node) in node.into_iter() {
-            let label = std::str::from_utf8(node.label()).unwrap().to_owned();
-            if level == 0 {
-                continue;
-            }
-            while level + 1 <= stack.len() {
-                let level = stack.len() - 2;
-                let (key, val) = stack.pop().unwrap();
-                stack[level].1.insert(key, val.into());
-            }
-            let mut val = JSONMap::new();
-            if level + 1 > stack.len() {
-                if let Some(nodes) = node.value() {
-                    let mut tree = HashMap::new();
-                    for (small_id, field_id) in nodes {
-                        let subtree = match tree.entry(field_id) {
-                            Entry::Occupied(o) => o.into_mut(),
-                            Entry::Vacant(v) => v.insert(HashMap::<usize, usize>::new()),
-                        };
-                        match subtree.entry(*small_id) {
-                            Entry::Occupied(o) => *o.into_mut() += 1,
-                            Entry::Vacant(v) => {
-                                v.insert(1);
-                            }
-                        };
-                    }
-                    for (small_id, counts) in tree.into_iter() {
-                        let df = counts.len();
-                        let mut ds = JSONMap::new();
-                        for (field_id, count) in counts.into_iter() {
-                            ds.insert(field_id.to_string(), count.into());
-                        }
-                        val.insert(
-                            "".to_string(),
-                            json!({
-                                small_id.to_string(): {
-                                    "df": df,
-                                    "ds": ds,
-                                }
-                            })
-                            .into(),
-                        );
-                    }
-                }
-                stack.push((label, val));
-            }
-        }
-        while stack.len() > 1 {
-            let level = stack.len() - 2;
-            let (key, val) = stack.pop().unwrap();
-            stack[level].1.insert(key, val.into());
-        }
-        index.insert("_tree".to_string(), stack.pop().unwrap().1.into());
-        h.insert("index".to_string(), index.into());
 
         return serde_json::to_string(&JSONValue::Object(h)).unwrap();
     }
